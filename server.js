ae36ec528db33b5cc884485000c0465711ad98c3
kritistha <kritistha020@gmail.com>
2024-09-30 12:56:05 +1000

    add few api endpoints

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
-
 const express = require("express");
 const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
@@ -10,13 +10,40 @@ const serverPort = process.env.SERVER_PORT;
 app.use(cors());
 app.use(express.json());
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong! Try again" });
+app.get("/error", (req, res, next) => {
+  const error = new Error("Internal Server Error");
+  error.status = 500;
+  next(error);
+});
+
+app.use((err, _req, res, _next) => {
+  //params need to be in this order and needs to include them all
+  //the .use middleware needs to be placed AFTER the route handlers
+  res.status(500);
+  res.json({ error: err.message || "Something went wrong" });
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from appy dappy!" });
+  res.json({ message: "Hello from my app!" });
+});
+
+app.get("/status", (req, res) => {
+  res.send("Status is all good.");
+});
+
+app.get("/person", (req, res) => {
+  // http://localhost:8000/person
+  res.send([1, 2, 3]);
+});
+
+app.get("/person/:id", (req, res) => {
+  // http://localhost:8000/person/1
+  res.send(req.params.id);
+});
+
+app.get("/person/:birthMonth/:birthYear", (req, res) => {
+  // http://localhost:8000/person/12/5?sortBy=name
+  res.send(req.query);
 });
 
 app.listen(serverPort, () => {
